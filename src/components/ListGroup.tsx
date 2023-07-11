e2fe129a86517e9337d549c11141a522973b022d
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-07-11 15:44:55 +0500

    Fragment

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,7 @@
 function ListGroup() {
-  return (
+  return ( 
+    <>
+        <h1>list</h1>
           <ul className="list-group">
           <li className="list-group-item">An item</li>
           <li className="list-group-item">A second item</li>
@@ -7,6 +9,7 @@ function ListGroup() {
           <li className="list-group-item">A fourth item</li>
           <li className="list-group-item">And a fifth one</li>
         </ul>
+    </>
   );
 }
 
