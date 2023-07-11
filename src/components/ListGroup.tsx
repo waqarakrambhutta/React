d48bcfe0c8827cd6d59dfe6462b0b6059a2b5c0f
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-07-11 16:58:51 +0500

    Conditional rendering.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,19 @@
 function ListGroup() {
-  const items = ["Pakistan", "Multan", "Lahore", "Islamabad", "Karachi"];
+  let items = ["Pakistan", "Multan", "Lahore", "Islamabad", "Karachi"];
+  items = [];
+
+  // if (items.length===0) // this method will cause code duplication. antoher way.
+  //     return (<><h1>List</h1><p>No items found</p></>)
+  // Or we can also store tenary operator in the variable and store it in JSX
 
   return (
     <>
       <h1>list</h1>
+
+      {/* {items.length===0 && <p>No items found.</p> /*this is another way*/}
+
+      {items.length === 0 ? <p>No items found</p> : null}
+
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
