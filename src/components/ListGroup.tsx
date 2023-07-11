a8d525212e3161247fc25fdebb3aa59a1b531998
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-07-11 16:39:31 +0500

    Rendering List

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,14 @@
 function ListGroup() {
-  return ( 
+  const items = ["Pakistan", "Multan", "Lahore", "Islamabad", "Karachi"];
+
+  return (
     <>
-        <h1>list</h1>
-          <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
-        </ul>
+      <h1>list</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </>
   );
 }
