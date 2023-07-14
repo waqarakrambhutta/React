263d0ec312a49bb8b1e14eb107582be3fbe870c1
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-07-14 20:27:56 +0500

    Showing an alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 function App() {
+  const [alertvisible,setAlertVisible] =useState(false)
+
   return (
     <div>
-      <Button color="warning" onClick={()=> console.log('Clicked') }>My button</Button>
-
+    {alertvisible && <Alert children="Warning" onClose={()=>{setAlertVisible(false)}}></Alert>}
+      <Button color="warning" onClick={()=> setAlertVisible(true) }>My button</Button>
     </div>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,13 +2,17 @@ import { ReactNode } from "react"
 
 interface Props{
     // text:string // this we will fail if we add the html content as child in the alert.tsx so we do.
-    children:ReactNode
+    children:ReactNode,
+    onClose: ()=>void
 }
 
-const Alert = ({children}:Props) => {
+const Alert = ({children, onClose}:Props) => {
   return (
-    <div className="alert alert-primary">{children}</div>
-    //class here we took from bootstrap Alert.
-  )
+
+    <div className="alert alert-primary alert-dismissible ">{children}
+      <button onClick={onClose} type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+
+    )
 }
 export default Alert
