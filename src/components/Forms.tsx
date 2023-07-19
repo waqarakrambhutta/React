191ac8e98fa672a44ca7c2a274f66e74e1ba5b8b
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-07-20 02:06:44 +0500

    Applying validation.

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -1,32 +1,41 @@
 import React, { FormEvent, useRef, useState } from "react";
 import { FieldValue, FieldValues, useForm } from "react-hook-form";
 
+interface formData{
+  name:string,
+  age:number
+}
+
 const Forms = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<formData>();
 
-  const {register, handleSubmit} = useForm()
-
-  const onSubmit = (data:FieldValues) =>console.log(data)
+  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
-    // <form onSubmit={handleSubmit(data=>console.log(data))}> // we can also use this technique.
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          {...register('name')}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
-        />
+          />
+          {errors.name?.type === "required" && <p className="text-danger">This field is required</p>}
+          {errors.name?.type==="minLength" && <p className="text-danger">The name should've 3 characters</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          {...register('age')}
+          {...register("age")}
           id="age"
           type="number"
           className="form-control"
