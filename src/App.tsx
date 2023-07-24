8ddd96e42e920cb1b2b0cafb10e21294a25cefa2
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-07-24 19:24:13 +0500

    Fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import { useEffect, useRef, useState } from "react";
 import ProductList from "./components/ProductList";
+import axios from "axios";
+
+interface User {
+  id: number;
+  name: string;
+}
+// we used interface for looking fields easily in the .then() function.
 
 function App() {
+  const [users, setusers] = useState<User[]>([]);
 
-  const [category,setcategory]=useState('')
+  useEffect(() => {
+    // here is the concept of promises understand it
+    // also log this on browser and see the type of data
 
-  const connect = ()=> {console.log('Connecting');}
-  const disconnect = ()=> {console.log('disconnecting');}
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((response) => setusers(response.data));
+  });
 
-  useEffect(()=>{
-    connect();
-    return ()=>{disconnect()}
-  })
-
-  return (
-    <div></div>
-  );
+  return <ul>
+    {users.map(user=><li key={user.id}>{user.name}</li>)}
+  </ul>
 }
 export default App;
